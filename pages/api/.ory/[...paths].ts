ed2d78632decde1f33a17ead9d9089a1d36f168a
Lina <lina.lumburovska@kern.ai>
2023-04-20 17:55:06 +0200

    Ory elements and login page

--- pages/api/.ory/[...paths].ts
+++ pages/api/.ory/[...paths].ts
@@ -1,13 +1,13 @@
-// @ory/integrations offers a package for integrating with Next.js in development. It is not needed in production.
-// @ory/integrations works in a similar way as ory tunnel, read more about it what it does:
-// https://www.ory.sh/docs/guides/cli/proxy-and-tunnel
+// @ory/integrations offers a package for integrating with NextJS.
 import { config, createApiHandler } from "@ory/integrations/next-edge"
 
 // We need to export the config.
 export { config }
 
-// And create the Ory Network API "bridge".
+// And create the Ory Cloud API "bridge".
 export default createApiHandler({
-    fallbackToPlayground: true,
-    dontUseTldForCookieDomain: true,
+  fallbackToPlayground: true,
+  // Because vercel.app is a public suffix and setting cookies for
+  // vercel.app is not possible.
+  dontUseTldForCookieDomain: true,
 })
