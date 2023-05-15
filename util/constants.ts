42cc75ba094278557085b61006bb5b316d659203
Lina <lina.lumburovska@kern.ai>
2023-05-15 14:00:52 +0200

    Flipped IS_OS variable

--- util/constants.ts
+++ util/constants.ts
@@ -1,2 +1,2 @@
-export const isFreeTrial = process.env.IS_OS === 'true' || process.env.IS_OS === '1';
+export const isFreeTrial = !(process.env.IS_OS === 'true' || process.env.IS_OS === '1');
 export const isDemoUser = process.env.IS_DEMO === 'true' || process.env.IS_DEMO === '1';
