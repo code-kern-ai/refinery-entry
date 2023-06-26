19db883a8f7546d4ca2131650b44da18b1640a99
Lina <lina.lumburovska@kern.ai>
2023-06-26 15:25:41 +0200

    Use submodule in entry

--- services/basic-fetch/util.ts
+++ services/basic-fetch/util.ts
@@ -1,3 +1,4 @@
+import { jsonFetchWrapper } from "@/submodules/javascript-functions/basic-fetch";
 import { MiscInfo } from "./misc";
 
 export enum FetchType {
@@ -7,32 +8,13 @@ export enum FetchType {
     DELETE = "DELETE",
 }
 
-export function jsonFetchWrapper(url: string, fetchType: FetchType, onResult?: (result: any) => void, body?: BodyInit, headers?: any, onError?: (response: any) => void) {
+export function jsonFetchWrapperChecksDemo(url: string, fetchType: FetchType, onResult?: (result: any) => void, body?: BodyInit, headers?: any, onError?: (response: any) => void) {
     if (!canRequestInDemoAccess(fetchType)) {
         console.log("Cannot request in demo access")
         alertUser();
         return;
     }
-    if (!headers) headers = {};
-    headers["Content-Type"] = "application/json";
-
-    let hasError = false;
-    let myFetch = fetch(url, {
-        method: fetchType,
-        headers: headers,
-        body: body,
-    }).then(response => {
-        if (!response.ok) {
-            if (onError) onError(response);
-            else throw new Error("Error in request at " + url);
-            hasError = true;
-        }
-        else return response.json();
-    });
-
-    if (onResult && !hasError) myFetch.then(result => onResult(result));
-
-
+    jsonFetchWrapper(url, fetchType, onResult, body, headers, onError);
 }
 
 
--- util/data-fetch.ts
+++ util/data-fetch.ts
@@ -1,11 +1,11 @@
-import { FetchType, jsonFetchWrapper } from "@/services/basic-fetch/util";
+import { FetchType, jsonFetchWrapperChecksDemo } from "@/services/basic-fetch/util";
 
 export function getIsManaged(onResult: (result: any) => void) {
     const url = `/is_managed`;
-    jsonFetchWrapper(url, FetchType.GET, onResult);
+    jsonFetchWrapperChecksDemo(url, FetchType.GET, onResult);
 }
 
 export function getIsDemo(onResult: (result: any) => void) {
     const url = `/is_demo`;
-    jsonFetchWrapper(url, FetchType.GET, onResult);
+    jsonFetchWrapperChecksDemo(url, FetchType.GET, onResult);
 }
