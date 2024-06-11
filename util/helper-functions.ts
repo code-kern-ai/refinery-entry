88daed1f27ae4632a28097bad87a407baaddc8fb
Lennart Schmidt <150007074+LennartSchmidtKern@users.noreply.github.com>
2024-06-11 08:50:52 +0200

    opt chaining (#14)

--- util/helper-functions.ts
+++ util/helper-functions.ts
@@ -49,7 +49,7 @@ export function prepareNodes(flow: any) {
         lastNameNode.attributes.required = true
     }
 
-    let filteredNodes = flow.ui.nodes.filter((node: any) => !node.attributes?.name.startsWith("metadata"));
+    let filteredNodes = flow.ui.nodes.filter((node: any) => !node.attributes?.name?.startsWith("metadata"));
 
     const providerId = flow.identity?.metadata_public?.registration_scope?.provider_id;
     if (["microsoft", "google"].includes(providerId)) {
