0583176a430a7cefe7e412b1e8bd44f05bf02c47
Lina <lina.lumburovska@kern.ai>
2024-06-20 17:32:28 +0200

    Added admin messages on refinery entry

--- util/data-fetch.ts
+++ util/data-fetch.ts
@@ -1,5 +1,8 @@
 import { FetchType, jsonFetchWrapper } from "@/submodules/javascript-functions/basic-fetch";
 
+const BACKEND_BASE_URI = '/refinery-gateway';
+export const organizationEndpoint = `${BACKEND_BASE_URI}/api/v1/organization`;
+
 export function getIsManaged(onResult: (result: any) => void) {
     const url = `/is_managed`;
     jsonFetchWrapper(url, FetchType.GET, onResult);
@@ -8,4 +11,14 @@ export function getIsManaged(onResult: (result: any) => void) {
 export function getIsDemo(onResult: (result: any) => void) {
     const url = `/is_demo`;
     jsonFetchWrapper(url, FetchType.GET, onResult);
+}
+
+export function getUserInfoExtended(onResult: (result: any) => void) {
+    const finalUrl = `${organizationEndpoint}/get-user-info-extended`;
+    jsonFetchWrapper(finalUrl, FetchType.GET, onResult);
+}
+
+export function getAllActiveAdminMessages(onResult: (result: any) => void) {
+    const finalUrl = `${organizationEndpoint}/all-active-admin-messages`
+    jsonFetchWrapper(finalUrl, FetchType.GET, onResult)
 }
