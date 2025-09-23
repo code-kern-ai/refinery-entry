d2216d134b85f193ce8ad4ee1c9a2a9e6c6e4bcc
Lina <lina.lumburovska@kern.ai>
2025-09-23 15:33:33 +0200

    UI changes

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -356,7 +356,7 @@ const Settings: NextPage = () => {
       </div>
       <div className="img-container">
       </div>
-      <AutoLogoutProgressBar ref={autoLogoutRef} className='absolute right-2 top-2' autoLogoutMinutes={user?.autoLogoutMinutes} preventLogout={true} />
+      {(language && loadPage) && <AutoLogoutProgressBar ref={autoLogoutRef} className='absolute right-2 top-2' autoLogoutMinutes={user?.autoLogoutMinutes} preventLogout={true} />}
       <AdminMessages
         adminMessages={activeAdminMessages}
         setActiveAdminMessages={setActiveAdminMessages} />
