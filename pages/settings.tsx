ed03670b8b52a8be9692bbb91616008f8f9d45b9
Lina <lina.lumburovska@kern.ai>
2025-09-23 15:46:24 +0200

    Label sent as param

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -356,7 +356,7 @@ const Settings: NextPage = () => {
       </div>
       <div className="img-container">
       </div>
-      {(language && loadPage) && <AutoLogoutProgressBar ref={autoLogoutRef} className='absolute right-2 top-2' autoLogoutMinutes={user?.autoLogoutMinutes} preventLogout={true} />}
+      {(language && loadPage) && <AutoLogoutProgressBar ref={autoLogoutRef} className='absolute right-2 top-2' autoLogoutMinutes={user?.autoLogoutMinutes} preventLogout={true} label={t("overview.remainingTime")} />}
       <AdminMessages
         adminMessages={activeAdminMessages}
         setActiveAdminMessages={setActiveAdminMessages} />
