515dc6772d212580304e65f6258f6f4e5f8c4b11
Lina <lina.lumburovska@kern.ai>
2025-09-24 15:28:40 +0200

    test

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -72,6 +72,7 @@ const Settings: NextPage = () => {
           WebSocketsService.initWsNotifications();
         }
         setLanguage(res?.languageDisplay);
+        console.log("res?.autoLogoutMinutes", res?.autoLogoutMinutes)
         if (res?.autoLogoutMinutes) {
           localStorage.setItem("comesFromEntry", "true");
         }
